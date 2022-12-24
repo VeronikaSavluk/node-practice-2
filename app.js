21653dd2e4c9a6f862ff24104478d0b2a5db0cdb
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2022-12-24 14:59:18 +0200

    start

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require("dotenv").config();
+const {PORT, DB_HOST} = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/contacts', contactsRouter);
+
+mongoose.connect(DB_HOST, () => console.log("DB is running"));
+app.listen(PORT, () => console.log("Server is running"));
---
+++ model/contacts.js
@@ -0,0 +1,16 @@
+const {model, Schema, Schema} = require('mongoose');
+
+const contactSchema = Schema({
+	name: {
+		type: String,
+	},
+	number: {
+		type: String,
+	},
+});
+
+const Contact = model('contact', contactSchema);
+
+module.exports = {
+	Contact
+};
